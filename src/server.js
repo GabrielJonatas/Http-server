77d447718ceb5940390a922db6ddf1ac558d8a7f
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2025-07-20 16:39:37 -0300

    feat: Implement request listener for books and authors in routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
+const requestListener = require('./routes.js');
+
 require('dotenv').config();
 const http = require('http');
 const PORT = process.env.PORT;
 const HOST = process.env.HOST
 
-const requestListener = (req,res) => {
-  res.writeHead(200);
-  res.end('Hello world!\n');
-};
+// const requestListener = (req,res) => {
+//   res.writeHead(200);
+//   res.end('Hello world!\n');
+// };
 
 const server = http.createServer(requestListener);
 
